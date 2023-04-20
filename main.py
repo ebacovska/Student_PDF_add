a5a75f61b68aee652e4e70d556d99cec9d8e7b6b
Elen Bačovská <elen.bacovska@gmail.com>
2023-04-20 11:35:30 +0200

    Add more pages for each toopic

--- main.py
+++ main.py
@@ -11,5 +11,7 @@ for inxex, row in  df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(10.0, 21.0, 200.0, 21.0)
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
 
 pdf.output("output.pdf")

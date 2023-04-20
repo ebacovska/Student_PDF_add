0fda00b004fc8feb57375a0a502d8271fc94d4f3
Elen Bačovská <elen.bacovska@gmail.com>
2023-04-20 15:19:43 +0200

    Add lines

--- main.py
+++ main.py
@@ -12,7 +12,10 @@ for inxex, row in  df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10.0, 21.0, 200.0, 21.0)
+    x = list(range(21,250, 5))
+
+    for i in x:
+        pdf.line(10.0, i, 200.0, i)
 
     pdf.ln(265)
     pdf.set_font(family="Times", style="I", size=8)
@@ -25,5 +28,9 @@ for inxex, row in  df.iterrows():
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+
+        for v in x:
+            pdf.line(10.0, v, 200.0, v)
 
 pdf.output("output.pdf")
